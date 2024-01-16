9ed21cb6bc52f11786b87d7ca6ac90b96239d32f
Givness <vlad-ermak107555@mail.ru>
2024-01-16 20:50:49 +1000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var gulp = require('gulp'),
+    gp = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+gulp.task('pug', function() {
+    return gulp.src(['app/pug/*.pug', '!app/pug/_*.pug'])
+        .pipe(gp.pug({
+            pretty:true
+        }))
+        .pipe(gulp.dest('dist'))
+        .on('end', browserSync.reload);
+});
+
+gulp.task('stylus', function() {
+    return gulp.src('app/styl/*.styl')
+        .pipe(gp.sourcemaps.init())
+        .pipe(gp.stylus({linenos: true}))
+        //.pipe(gp.autoprefixer({browsers: ['last 2 versions']}))
+        .on('error', gp.notify.onError({title: 'style'}))
+        .pipe(gp.csso())
+        .pipe(gp.sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({
+            stream:true
+        }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/pug/*.pug', gulp.series('pug'))
+    gulp.watch('app/styl/*.styl', gulp.series('stylus'))
+});
+
+gulp.task('default', gulp.series(
+    gulp.parallel('pug', 'stylus'),
+    gulp.parallel('watch', 'serve')
+));
